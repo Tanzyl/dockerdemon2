689eaa6fc213d5c6555c561e73fc3b2ce8ac08f5
Your Name <f201023@cfd.nu.edu.pk>
2023-11-21 09:08:01 +0500

    Phase 4 project

---
+++ Phase 4/superset_config.py
@@ -0,0 +1,5 @@
+# superset_config.py
+SECRET_KEY = 'vQmRkwpRNm5u_6IKXBX9739bXkhiueMF'
+
+# Database connection information
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://odoo:odoo@db:5432/postgres'
